dd55c969f3ddbec58218a7d59c239c05df196ee2
afifym <mohamed.afify323@gmail.com>
2021-05-23 23:51:10 +0200

    fixed type errors

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -1,31 +1,8 @@
-// import { ITodo } from '../config/interfaces';
 import { ITag, ITodosState } from '../config/interfaces';
 import firebase from './config';
-
 const db = firebase.firestore();
 
-// const getTodos = async (uid: string) => {
-//   const data: any = await db
-//     .collection('users')
-//     .doc(uid)
-//     .collection('todos')
-//     .get();
-//   const todos = data.docs.map((doc) => doc.data());
-
-//   console.log('fetch: ', todos);
-//   return todos;
-// };
-
-// const updateTodoTitle = async (todo: ITodo, title: string) => {
-//   db.collection('spells')
-//     .doc(todo.id)
-//     .set({ ...todo, title });
-// };
-
-// }
-export const getUserData = async (uid) => {
-  //   const userRef = db.doc(`users/${user.uid}`);
-
+export const getUserData = async (uid: string) => {
   const ref = await db.collection('users').doc(uid);
   const refGet = await ref.get();
 
@@ -45,27 +22,14 @@ export const getUserData = async (uid) => {
       { merge: true }
     );
   }
-
   const data = await (await ref.get()).data();
-
-  //   ref.get().then((snapshot: any) => console.log('snap: ', snapshot.docs));
-  //   const setted = ref.set({ todos: [], tags: [] }, { merge: true });
-
-  //   const s = ref
-  //     .set({ todos: [], tags: [] }, { merge: true })
-  //     .then((d) => console.log('d: ', d));
-
-  //   console.log('S: ', s);
-  // return ref.data();
   return data;
 };
 
 export const updateTodos = async (uid: string, todos: ITodosState) => {
-  const ref = await db.collection('users').doc(uid).update({ todos: todos });
-  console.log('UPDATE');
+  await db.collection('users').doc(uid).update({ todos: todos });
 };
 
 export const updateTags = async (uid: string, tags: ITag[]) => {
-  const ref = await db.collection('users').doc(uid).update({ tags: tags });
-  console.log('UPDATE');
+  await db.collection('users').doc(uid).update({ tags: tags });
 };
